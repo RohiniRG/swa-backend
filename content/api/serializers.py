356d07110509b141d99800cd552c54844d37752e
Rohini Rao <rohinirao2112@gmail.com>
2022-04-05 23:06:41 +0530

    Serializer modified

--- content/api/serializers.py
+++ content/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from content.models import Content
 
 class ContentSerializer(serializers.ModelSerializer):
+    created_by = serializers.CharField()
     class Meta:
         model = Content
         fields = ["id", "title", "content", "tags", "image", "created_at", "updated_at", "is_archived", "created_by"]
