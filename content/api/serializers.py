2f635523db1f88a1a5294b9cf9c83fec5e9d950a
Rohini Rao <rohinirao2112@gmail.com>
2022-04-06 07:58:15 +0530

    Content apis error resolved

--- content/api/serializers.py
+++ content/api/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from content.models import Content
 
-class ContentSerializer(serializers.ModelSerializer):
-    created_by = serializers.CharField()
+class ContentSerializer(serializers.ModelSerializer):    
+    created_by = serializers.CharField(source="created_by.username", read_only=True)
     class Meta:
         model = Content
         fields = ["id", "title", "content", "tags", "image", "created_at", "updated_at", "is_archived", "created_by"]
-        read_only_fields = ["id", "created_at", "updated_at", "created_by", "is_archived"]
+        read_only_fields = ["id", "created_at", "updated_at", "is_archived", "created_by"]
 
